49acdbfb20f9d02cae257523372b801cdcf43d7d
Manolo Perez <manolope338@gmail.com>
2019-05-06 00:53:05 -0500

    Add structure based on the line-bot-sdk

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 API_KEY_FILENAME = os.environ.get('API_KEY_FILENAME')
 SHEET_NAME = os.environ.get('SHEET_NAME')
+CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
+CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
+PORT = os.environ.get('PORT', 5000)
