6ae9e68b28f2ce5b95009586f7c2f379dce30d0d
D. Reinecke <36729742+Reinre@users.noreply.github.com>
2019-05-08 01:09:47 +0900

    Class creation

--- sales_report.py
+++ sales_report.py
@@ -1,7 +1,21 @@
+
 class SalesReport:
     """Class to pack the query data, to be shown on the message"""
-    def __init__(self, goal_status, goal_color, goal, sales):
+    def __init__(self, goal_status, goal_color, dates, last_year, saless, goal, sanji, rokuji, kuji, change_txt, changes, monthly_goal, currents, lefts, progress):
         self.goal_status = goal_status
         self.goal_color = goal_color
+        self.dates = dates
+        self.last_year = last_year
+        self.saless = saless
         self.goal = goal
-        self.sales = sales
+        self.sanji = sanji
+        self.rokuji = rokuji
+        self.kuji = kuji
+        self.change_txt = change_txt
+        self.changes = changes
+        self.monthly_goal = monthly_goal
+        self.currents = currents
+        self.lefts = lefts
+        self.progress = progress
+        self.sales = sales 
+        
