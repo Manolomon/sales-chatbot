acafa154eb2b824eb28e54585fb9f84345d85303
Manolo Perez <manolope338@gmail.com>
2019-05-08 21:19:06 -0500

    Push messages

--- app.py
+++ app.py
@@ -28,14 +28,10 @@ template_env = Environment(
     autoescape=select_autoescape(['html', 'xml', 'json'])
 )
 
+users = []
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    """
-    This is the main method that gets called every time someone 
-    sends a message to the bot. Here the method handle_message 
-    gets invoked. TODO: Messages that
-    are not triggered buy the user need another kind of method
-    """
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
@@ -50,23 +46,25 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    """
-    Method to send the response message. TODO: Here, is 
-    possible to read the user message to work with wome input
-    """
-    # Create an object SalesReport to pack the data tha is going
-    #  to be sent to the json template
+    sender_id = event.source.user_id
+    if sender_id not in users:
+        users.append(sender_id)
     data = SalesReport('REACHED', '#1DB446', '$3000', '$5557');
     template = template_env.get_template('sales-report.json')
     data = template.render(dict(data=data))
-    # Send the structured message. Note, the template needs to be parsed to 
-    # an Flex MessageUI element, in this case a BubbleContainer
-    message = FlexSendMessage(alt_text = "Alt Text", contents=BubbleContainer.new_from_json_dict(json.loads(data)))
+    message = FlexSendMessage(alt_text = "Sales Report", contents=BubbleContainer.new_from_json_dict(json.loads(data)))
     line_bot_api.reply_message(event.reply_token, message)
+    send_push_message()
+
+def send_push_message():
+    data = SalesReport('TO BE REACHED', '#FFC107', '$5557', '$2322');
+    template = template_env.get_template('sales-report.json')
+    data = template.render(dict(data=data))
+    message = FlexSendMessage(alt_text = "Sales Report", contents=BubbleContainer.new_from_json_dict(json.loads(data)))
+    for user in users:
+        line_bot_api.push_message(user, message)
 
 import os
-
-# Initial method to be run by the server
 if __name__ == "__main__":
-    port = int(config.PORT)
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
