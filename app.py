d46138a040cf01f1f00facf724fa2d3cc881722b
Manolo Perez <31350932+Manolomon@users.noreply.github.com>
2019-05-08 21:35:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,33 @@ template_env = Environment(
 )
 
 users = []
+global month
+	global current
+	global left
+	global monthlyObj
+	global objList
+	global pastList
+	global currentDate
+
+	global objectivestatus_o 	
+	global date_o 
+
+	global lastYearSales_o
+	global objective_o 
+	global totalSales_o 
+
+	global threepm_o 
+	global sixpm_o 
+	global ninepm_o 
+
+	global changetxt_o 
+	global change_o 
+
+	global monthlyObj_o 
+	global current_o 
+
+	global left_o 
+	global percentage_o 
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -67,12 +94,12 @@ def handle_message(event):
     data = template.render(dict(data=data))
     # Send the structured message. Note, the template needs to be parsed to 
     # an Flex MessageUI element, in this case a BubbleContainer
-    message = FlexSendMessage(alt_text = "Alt Text", contents=BubbleContainer.new_from_json_dict(json.loads(data)))
+    message = FlexSendMessage(alt_text = "Sales Report", contents=BubbleContainer.new_from_json_dict(json.loads(data)))
     line_bot_api.reply_message(event.reply_token, message)
     send_push_message()
 
 def send_push_message():
-    data = SalesReport('TO BE REACHED', '#FFC107', '$5557', '$2322');
+    data = SalesReport(objectivestatus_o, '#FFC107', date_o, lastYearSales_o, totalSales_o, objective_o, threepm_o, sixpm_o, ninepm_o, change_o, monthlyObj_o, current_o, left_o, percentage_o);
     template = template_env.get_template('sales-report.json')
     data = template.render(dict(data=data))
     message = FlexSendMessage(alt_text = "Sales Report", contents=BubbleContainer.new_from_json_dict(json.loads(data)))
@@ -83,52 +110,3 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-# Template format definition
-
-template_env = Environment(
-    loader=FileSystemLoader('templates'),
-    autoescape=select_autoescape(['html', 'xml', 'json'])
-)
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    """
-    This is the main method that gets called every time someone 
-    sends a message to the bot. Here the method handle_message 
-    gets invoked. TODO: Messages that
-    are not triggered buy the user need another kind of method
-    """
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-
-
-
-
-
-
-
-# Now now, I have no idea what im doing but please find a way to send scheduled push messages 
-# and retrieve the user input to store it as variables threepm, sixpm, ninepm and totalSales
-
-
-
-
-
-
-
-
-
-
-
-
